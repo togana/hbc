50b8396f2039726e2f8eb012d36cf3ddc33d1595
togana <o.togana@gmail.com>
2021-07-22 02:20:07 +0900

    画像URLが許可してないドメインも返却されていたので許可リストに追加した

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn2.thecatapi.com', '30.media.tumblr.com'],
+    domains: [
+      'cdn2.thecatapi.com',
+      '30.media.tumblr.com',
+      '28.media.tumblr.com',
+    ],
   },
 };
